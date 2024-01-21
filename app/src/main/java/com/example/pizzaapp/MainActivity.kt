63308627ff602b578af9c512bf799e70986696ce
oscar066 <oscarkaruga1@gmail.com>
2024-01-21 20:14:59 +0300

    Added the PizzaBuilderScreen

--- app/src/main/java/com/example/pizzaapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaapp/MainActivity.kt
@@ -3,18 +3,13 @@ package com.example.pizzaapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pizzaapp.model.Topping
-import com.example.pizzaapp.model.ToppingPlacement
-import com.example.pizzaapp.ui.ToppingCell
+import com.example.pizzaapp.ui.PizzaBuilderScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToppingCell(
-                topping = Topping.Pepperoni,
-                placement = ToppingPlacement.Left
-            ) {}
+            PizzaBuilderScreen()
         }
     }
 }
--- app/src/main/java/com/example/pizzaapp/ui/ToppingCell.kt
+++ app/src/main/java/com/example/pizzaapp/ui/ToppingCell.kt
@@ -1,13 +1,20 @@
 package com.example.pizzaapp.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pizzaapp.model.Topping
 import com.example.pizzaapp.model.ToppingPlacement
 
@@ -33,14 +40,29 @@ private fun ToppingCellPreview() {
 fun ToppingCell(
     topping: Topping,
     placement: ToppingPlacement?,
+    modifier: Modifier = Modifier,
     onClickTopping: () -> Unit
 ) {
-    Row {
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .clickable { onClickTopping() }
+            .padding(
+            vertical = 4.dp,
+            horizontal = 16.dp )
+
+
+    ){
+
         Checkbox(
             checked = (placement != null),
             onCheckedChange = { /* TODO */ }
         )
-        Column {
+        Column (
+            modifier = Modifier.weight(1f, fill = true)
+                .padding(start = 4.dp)
+
+        ){
 
             Text(
                 text = stringResource(topping.toppingName),
